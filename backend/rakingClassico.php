6e470d7fa65aa535f88d7f7f0cf4dd7bce7c382e
gbrrs <g260789@dac.unicamp.br>
2023-11-22 22:29:25 -0300

    versao final ranking estatico

--- backend/rakingClassico.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // backend_ranking_classico.php
-    include(conexao.php);
-
-    $sql_ranking_classico = "SELECT nome, pontuacao, nivel, tempoPartida FROM ranking ORDER BY pontuacao DESC LIMIT 10";
-
-    $script_ranking_classico = $connection->exec($sql_ranking_classico);
-
-?>
